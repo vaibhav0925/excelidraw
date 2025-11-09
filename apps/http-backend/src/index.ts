867985fcb9c278a6819b19a14d0624fa4e80b903
Vaibhav Shukla <vaibhav.shukla0925@gmail.com>
2025-11-09 17:10:45 +0530

    Added SignUp Endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,17 +3,32 @@ import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/config" 
 import { middleware } from "./middleware"
 import {CreateUserSchema, SignInSchema, CreateRoomSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/db/client"
 const app = express()
 
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
     
-    const data = CreateUserSchema.safeParse(req.body)
-    if(!data.success){
+    const parsedData = CreateUserSchema.safeParse(req.body)
+    if(!parsedData.success){
         return res.json({
             messages: "Incorrect Input"
         })
     }
+    try{
+        await prismaClient.user.create({
+            data:{
+                email: parsedData.data?.username,
+                password: parsedData.data?.password,
+                name: parsedData.data?.name
+            }
+        })
+    }catch(e){
+        res.status(401).json({
+            message: "User Already Exist With this username"
+        })
+    }
+    
 
     res.json({
         userId: "123"
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+export const prismaClient = new PrismaClient();
